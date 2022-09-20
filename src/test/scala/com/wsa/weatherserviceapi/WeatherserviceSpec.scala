798011177d30828cf37b8f3b18b29093c771ed74
Nic Flores <flores314@gmail.com>
2022-09-20 11:35:58 -0600

    refactor api test to use checkWeatherApiStatus

--- src/test/scala/com/wsa/weatherserviceapi/WeatherserviceSpec.scala
+++ src/test/scala/com/wsa/weatherserviceapi/WeatherserviceSpec.scala
@@ -1,40 +1,47 @@
 package com.wsa.weatherserviceapi
 
 import cats.effect.IO
+import cats.effect.Resource
 import org.http4s._
+import org.http4s.client.Client
+import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.implicits._
 import weaver.SimpleIOSuite
-import org.http4s.ember.client.EmberClientBuilder
-import cats.effect.Resource
-import org.http4s.client.Client
+import weaver.Expectations
 
 object WeatherserviceSpec extends SimpleIOSuite {
 
   val eClient: Resource[IO, Client[IO]] = EmberClientBuilder.default[IO].build
 
   test("Valid location returns 200") {
-    eClient.use { client =>
-      {
-        val nws = NWService[IO](client)
-        val wClient =
-          Client.fromHttpApp(WeatherserviceapiRoutes[IO](nws).routes.orNotFound)
-        wClient
-          .run(Request[IO](Method.GET, uri"/weather" / "40.7484,-73.9856"))
-          .use(resp => IO.pure(expect.same(resp.status, Status.Ok)))
-      }
-    }
+    checkWeatherApiStatus(
+      eClient,
+      uri"/weather" / "40.7484,-73.9856",
+      Status.Ok
+    )
   }
 
   test("Non supported locations returns 404") {
+    checkWeatherApiStatus(
+      eClient,
+      uri"/weather" / "77.6177,42.4944",
+      Status.NotFound
+    )
+  }
+
+  def checkWeatherApiStatus[A](
+      emberClient: Resource[IO, Client[IO]],
+      endpoint: Uri,
+      found: A
+  ): IO[Expectations] =
     eClient.use { client =>
       {
         val nws = NWService[IO](client)
         val wClient =
           Client.fromHttpApp(WeatherserviceapiRoutes[IO](nws).routes.orNotFound)
         wClient
-          .run(Request[IO](Method.GET, uri"/weather" / "77.6177,42.4944"))
-          .use(resp => IO.pure(expect.same(resp.status, Status.NotFound)))
+          .run(Request[IO](Method.GET, endpoint))
+          .use(resp => IO.pure(expect.same(resp.status, found)))
       }
     }
-  }
 }
